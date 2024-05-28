276d4a347b468a0e72ca1f8a6de66f20016fcf86
mahaut231 <60514549+mahaut231@users.noreply.github.com>
2024-05-28 14:32:13 +0200

    Create App.java

---
+++ backend/src/App.java
@@ -0,0 +1,7 @@
+import database.PolyBayDatabase;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        PolyBayDatabase test = new PolyBayDatabase();
+    }
+}
